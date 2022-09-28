3c6a3033485b7d8841653e60bd31bb708c892e00
Ignacio-antonio <yignaciosd@gmail.com>
2022-09-27 23:49:23 -0300

    initial commit

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars.githubusercontent.com/u/113483608?v=4" alt="Ignacio Antonio" />
+                        <div>
+                            <strong>Ignacio Antonio</strong>
+                            <span>Química</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Entusiasta das melhores tecnologias de química avançada.
+                        <br /><br />
+                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experéncias. 
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/Hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+    )
+}
+
+export default TeacherItem;
